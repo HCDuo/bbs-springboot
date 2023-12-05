face019473249d6ce6f15ffd33e25ac489edd73d
HCDUO <2650591287@qq.com>
2023-12-05 23:26:06 +0800

    Initial commit

---
+++ bbs-common/src/main/java/com/liang/bbs/common/util/Placeholder.java
@@ -0,0 +1,8 @@
+package com.liang.bbs.common.util;
+
+/**
+ * @author maliangnansheng
+ * @date 2021-06-13 16:08
+ */
+public class Placeholder {
+}
---
+++ bbs-user/bbs-user-service/src/main/java/com/liang/bbs/user/service/utils/Placeholder.java
@@ -0,0 +1,8 @@
+package com.liang.bbs.user.service.utils;
+
+/**
+ * @author maliangnansheng
+ * @date 2021-06-13 16:08
+ */
+public class Placeholder {
+}
